6690368dbd240690af9264cbdd63cafaf81be359
disami115 <disami115@yandex.ru>
2020-11-20 22:12:04 +0300

    Удалил тест testConnectorHasReference

--- tests/ComponentsTest/ConnectorTest.php
+++ tests/ComponentsTest/ConnectorTest.php
@@ -6,38 +6,31 @@ namespace Leadvertex\Plugin\Components\Db\ComponentsTest;
 
 use Leadvertex\Plugin\Components\Db\Components\Connector;
 use Leadvertex\Plugin\Components\Db\Components\PluginReference;
+use Leadvertex\Plugin\Components\Db\ModelTrait;
 use Medoo\Medoo;
 use PHPUnit\Framework\TestCase;
+use ReflectionClass;
 
 class ConnectorTest extends TestCase
 {
+    public static function setUpBeforeClass(): void
+    {
+        parent::setUpBeforeClass();
+        Connector::init(
+            new Medoo([
+                'database_type' => 'sqlite',
+                'database_file' => ':memory:'
+            ])
+        );
+    }
 
     public function testConnectorSetReference()
     {
-        Connector::init(
-            new Medoo([
-                'database_type' => 'sqlite',
-                'database_file' => ':memory:'
-            ])
-        );
         $pluginReference = new PluginReference(1, 'user', 2);
         Connector::setReference($pluginReference);
+
 
         $this->assertSame($pluginReference, Connector::getReference());
-    }
-
-    public function testConnectorHasReference()
-    {
-        Connector::init(
-            new Medoo([
-                'database_type' => 'sqlite',
-                'database_file' => ':memory:'
-            ])
-        );
-        $this->assertEquals(false, Connector::hasReference());
-        $pluginReference = new PluginReference(1, 'user', 2);
-        Connector::setReference($pluginReference);
-        $this->assertEquals(true, Connector::hasReference());
     }
 
 }
