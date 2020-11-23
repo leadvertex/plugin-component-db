202037b429932766567b52e3cfd4ffa94e8d2085
disami115 <disami115@yandex.ru>
2020-11-23 22:54:05 +0300

    Добавил тесты и перенес testGetReference

--- tests/ComponentsTest/ConnectorTest.php
+++ tests/ComponentsTest/ConnectorTest.php
@@ -8,27 +8,100 @@ use Leadvertex\Plugin\Components\Db\Components\Connector;
 use Leadvertex\Plugin\Components\Db\Components\PluginReference;
 use Medoo\Medoo;
 use PHPUnit\Framework\TestCase;
+use RuntimeException;
 
 class ConnectorTest extends TestCase
 {
-    public static function setUpBeforeClass(): void
+    public function testGetDb()
     {
-        parent::setUpBeforeClass();
+        $medoo = new Medoo([
+            'database_type' => 'sqlite',
+            'database_file' => ':memory:'
+        ]);
+        Connector::init($medoo);
+
+        $pluginReference = new PluginReference(1, 'user', 2);
+        Connector::setReference($pluginReference);
+        $this->assertSame($medoo, Connector::db());
+    }
+
+    public function testConnectorSetReference()
+    {
         Connector::init(
             new Medoo([
                 'database_type' => 'sqlite',
                 'database_file' => ':memory:'
             ])
         );
-    }
 
-    public function testConnectorSetReference()
-    {
         $pluginReference = new PluginReference(1, 'user', 2);
         Connector::setReference($pluginReference);
-
 
         $this->assertSame($pluginReference, Connector::getReference());
     }
 
+    public function testConnectorReferenceNotConfigured()
+    {
+        Connector::init(
+            new Medoo([
+                'database_type' => 'sqlite',
+                'database_file' => ':memory:'
+            ])
+        );
+        try {
+            $actual = Connector::getReference();
+        } catch (RuntimeException $e) {
+            $actual = $e->getMessage();
+        }
+
+        $this->assertSame('Plugin reference is not configured', $actual);
+    }
+
+    public function testConnectorMedooNotConfigured()
+    {
+        try {
+            $actual = Connector::db();
+        } catch (RuntimeException $e) {
+            $actual = $e->getMessage();
+        }
+
+        $this->assertSame('Medoo was not configured', $actual);
+    }
+
+    public function testGetReference()
+    {
+        Connector::init(
+            new Medoo([
+                'database_type' => 'sqlite',
+                'database_file' => ':memory:'
+            ])
+        );
+        $pluginReference = new PluginReference(1, 'user', 2);
+        Connector::setReference($pluginReference);
+
+        $this->assertEquals('user', $pluginReference->getAlias());
+        $this->assertEquals(1, $pluginReference->getCompanyId());
+        $this->assertEquals(2, $pluginReference->getId());
+        $this->assertEquals($pluginReference, new PluginReference(1, 'user', 2));
+    }
+
+    public function testConnectorHasReference()
+    {
+        Connector::init(
+            new Medoo([
+                'database_type' => 'sqlite',
+                'database_file' => ':memory:'
+            ])
+        );
+        $actual = Connector::hasReference();
+
+        $this->assertSame(false, $actual);
+
+        $pluginReference = new PluginReference(1, 'user', 2);
+        Connector::setReference($pluginReference);
+
+        $actual = Connector::hasReference();
+
+        $this->assertSame(true, $actual);
+    }
 }
