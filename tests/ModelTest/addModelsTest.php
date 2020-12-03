807f7bcfa9626d1c313ffc223c31f1cec1b07e03
disami115 <disami115@yandex.ru>
2020-12-03 23:03:25 +0300

    change tests addModelsTest

--- tests/ModelTest/addModelsTest.php
+++ tests/ModelTest/addModelsTest.php
@@ -8,6 +8,8 @@ use InvalidArgumentException;
 use Leadvertex\Plugin\Components\Db\Commands\CreateTablesCommand;
 use Leadvertex\Plugin\Components\Db\Components\Connector;
 use Leadvertex\Plugin\Components\Db\Components\PluginReference;
+use Leadvertex\Plugin\Components\Db\Components\TestAnotherPluginModelClass;
+use Leadvertex\Plugin\Components\Db\Components\TestAnotherSinglePluginModelClass;
 use Leadvertex\Plugin\Components\Db\Components\TestModelClass;
 use Leadvertex\Plugin\Components\Db\Components\TestModelWithAfterAndBeforeClass;
 use Leadvertex\Plugin\Components\Db\Components\TestModelWithArrayClass;
@@ -32,14 +34,14 @@ class addModelsTest extends TestCase
         );
 
         Connector::setReference(new PluginReference(1, 'user', 2));
+
+        $command = new CreateTablesCommand();
+        $tester = new CommandTester($command);
+        $tester->execute([]);
     }
 
     public function testAddTestModelClassAndFindById()
     {
-        $command = new CreateTablesCommand();
-        $tester = new CommandTester($command);
-        $tester->execute([]);
-
         $model = new TestModelClass();
         $model->setId(11);
         $model->value_1 = 11;
@@ -47,22 +49,49 @@ class addModelsTest extends TestCase
         $model->save();
 
         TestModelClass::freeUpMemory();
-        $result = var_export(TestModelClass::findById( 11), true);
-        $expected = "Leadvertex\Plugin\Components\Db\Components\TestModelClass::__set_state(array(
-   'value_1' => 11,
-   'value_2' => 'Hello world 11',
-   'id' => '11',
-   '_isNew' => false,
-))";
-        $this->assertSame(str_replace("\r", '', $expected), $result);
+        $result = TestModelClass::findById( 11);
+
+        $this->assertInstanceOf('Leadvertex\Plugin\Components\Db\Components\TestModelClass', $result);
+        $this->assertEquals(11, $result->getId());
+        $this->assertEquals(11, $result->value_1);
+        $this->assertEquals('Hello world 11', $result->value_2);
+    }
+
+    public function testAddTestPluginModelClassAndFindById()
+    {
+        $model = new TestPluginModelClass();
+        $model->setId(11);
+        $model->value_1 = 11;
+        $model->value_2 = 'Hello world 11';
+        $model->save();
+
+        TestPluginModelClass::freeUpMemory();
+        $result = TestPluginModelClass::findById( 11);
+
+        $this->assertInstanceOf('Leadvertex\Plugin\Components\Db\Components\TestPluginModelClass', $result);
+        $this->assertEquals(11, $result->getId());
+        $this->assertEquals(11, $result->value_1);
+        $this->assertEquals('Hello world 11', $result->value_2);
+    }
+
+    public function testAddTestSinglePluginModelClassAndFind()
+    {
+        $model = new TestSinglePluginModelClass();
+        $model->value_1 = 11;
+        $model->value_2 = 'Hello world 11';
+        $model->save();
+
+        TestSinglePluginModelClass::freeUpMemory();
+        $result = TestSinglePluginModelClass::find();
+
+        $this->assertInstanceOf('Leadvertex\Plugin\Components\Db\Components\TestSinglePluginModelClass', $result);
+        $this->assertEquals(2, $result->getId());
+        $this->assertEquals(11, $result->value_1);
+        $this->assertEquals('Hello world 11', $result->value_2);
     }
 
     public function testAddTestModelWithArrayClass()
     {
-        $command = new CreateTablesCommand();
-        $tester = new CommandTester($command);
-        $tester->execute([]);
-
         $model = new TestModelWithArrayClass();
         $model->setId(11);
         $model->value_1 = 11;
@@ -77,52 +106,37 @@ class addModelsTest extends TestCase
         }
 
         $expected = "Field 'value_2' of 'Leadvertex\Plugin\Components\Db\Components\TestModelWithArrayClass' should be scalar or null";
-        $this->assertSame(str_replace("\r", '', $expected), $result);
+        $this->assertEquals($expected, $result);
     }
 
     public function testAddTestModelClassGetId()
     {
-        $command = new CreateTablesCommand();
-        $tester = new CommandTester($command);
-        $tester->execute([]);
         $id = 11;
         $model = new TestModelClass();
         $model->setId($id);
-        $result = $model->getId();
-        $expected = $id;
-        $this->assertSame(str_replace("\r", '', $expected), $result);
+        $this->assertSame('11', $model->getId());
     }
 
     public function testAddTestModelWithAfterAndBeforeClass()
     {
-        $command = new CreateTablesCommand();
-        $tester = new CommandTester($command);
-        $tester->execute([]);
         $id = 11;
         $model = new TestModelWithAfterAndBeforeClass();
         $model->setId($id);
-        $model->value_1 = 1;
-        $model->value_2 = '2';
+        $model->value_1 = 11;
+        $model->value_2 = 'Hello world 11';
         $model->save();
         $this->assertEquals('Start save', TestModelWithAfterAndBeforeClass::$message);
         TestModelWithAfterAndBeforeClass::freeUpMemory();
-        $result = var_export(TestModelWithAfterAndBeforeClass::findById($id), true);
+        $result = TestModelWithAfterAndBeforeClass::findById($id);
         $this->assertEquals('Find complete', TestModelWithAfterAndBeforeClass::$message);
-        $expected = "Leadvertex\Plugin\Components\Db\Components\TestModelWithAfterAndBeforeClass::__set_state(array(
-   'value_1' => 1,
-   'value_2' => '2',
-   'id' => '11',
-   '_isNew' => false,
-))";
-        $this->assertEquals(str_replace("\r", '', $expected), $result);
+        $this->assertInstanceOf('Leadvertex\Plugin\Components\Db\Components\TestModelWithAfterAndBeforeClass', $result);
+        $this->assertEquals(11, $result->getId());
+        $this->assertEquals(11, $result->value_1);
+        $this->assertEquals('Hello world 11', $result->value_2);
     }
 
     public function testAddTestModelClassWithNotUniqueId()
     {
-        $command = new CreateTablesCommand();
-        $tester = new CommandTester($command);
-        $tester->execute([]);
-
         $model = new TestModelClass();
         $model->setId(22);
         $model->value_1 = 22;
@@ -141,17 +155,13 @@ class addModelsTest extends TestCase
         } catch (DatabaseException $e) {
             $result = $e->getMessage();
         }
-        $expected = '23000: UNIQUE constraint failed: TestModelClass.id
-INSERT INTO "TestModelClass" ("value_1", "value_2", "id") VALUES (1, \'Hello world 1\', \'22\')';
-        $this->assertSame(str_replace("\r", '', $expected), $result);
+        $expected = str_replace("\r", '', '23000: UNIQUE constraint failed: TestModelClass.id
+INSERT INTO "TestModelClass" ("value_1", "value_2", "id") VALUES (1, \'Hello world 1\', \'22\')');
+        $this->assertSame($expected, $result);
     }
 
     public function testAddTestPluginModelClassWithNotUniqueId()
     {
-        $command = new CreateTablesCommand();
-        $tester = new CommandTester($command);
-        $tester->execute([]);
-
         $model = new TestPluginModelClass();
         $model->setId(22);
         $model->value_1 = 22;
@@ -177,10 +187,6 @@ INSERT INTO "TestPluginModelClass" ("value_1", "value_2", "id", "companyId", "pl
 
     public function testAddTestSinglePluginModelClassWithNotUniqueId()
     {
-        $command = new CreateTablesCommand();
-        $tester = new CommandTester($command);
-        $tester->execute([]);
-
         $model = new TestSinglePluginModelClass();
         $model->value_1 = 6;
         $model->value_2 = 'Hello world 7';
@@ -200,6 +206,94 @@ INSERT INTO "TestPluginModelClass" ("value_1", "value_2", "id", "companyId", "pl
         $expected = '23000: UNIQUE constraint failed: TestSinglePluginModelClass.companyId, TestSinglePluginModelClass.pluginAlias, TestSinglePluginModelClass.pluginId, TestSinglePluginModelClass.id
 INSERT INTO "TestSinglePluginModelClass" ("value_1", "value_2", "id", "companyId", "pluginAlias", "pluginId") VALUES (1, \'Hello world 1\', \'2\', \'1\', \'user\', \'2\')';
         $this->assertSame(str_replace("\r", '', $expected), $result);
+    }
+
+    public function testAddTwoModelAndFind()
+    {
+        $model_1 = new TestModelClass();
+        $model_1->setId(1);
+        $model_1->value_1 = 1;
+        $model_1->value_2 = '1';
+        $model_1->save();
+
+        $model_2 = new TestModelWithAfterAndBeforeClass();
+        $model_2->setId(1);
+        $model_2->value_1 = 2;
+        $model_2->value_2 = '2';
+        $model_2->save();
+
+        TestModelClass::freeUpMemory();
+
+        $findModel_1 = TestModelClass::findById(1);
+        $findModel_2 = TestModelWithAfterAndBeforeClass::findById(1);
+
+        $this->assertInstanceOf('Leadvertex\Plugin\Components\Db\Components\TestModelClass', $findModel_1);
+        $this->assertEquals(1, $findModel_1->getId());
+        $this->assertEquals(1, $findModel_1->value_1);
+        $this->assertEquals('1', $findModel_1->value_2);
+
+        $this->assertInstanceOf('Leadvertex\Plugin\Components\Db\Components\TestModelWithAfterAndBeforeClass', $findModel_2);
+        $this->assertEquals(1, $findModel_2->getId());
+        $this->assertEquals(2, $findModel_2->value_1);
+        $this->assertEquals('2', $findModel_2->value_2);
+    }
+
+    public function testAddTwoPluginModelAndFind()
+    {
+        $model_1 = new TestPluginModelClass();
+        $model_1->setId(1);
+        $model_1->value_1 = 1;
+        $model_1->value_2 = '1';
+        $model_1->save();
+
+        $model_2 = new TestAnotherPluginModelClass();
+        $model_2->setId(1);
+        $model_2->value_1 = 2;
+        $model_2->value_2 = '2';
+        $model_2->save();
+
+        TestModelClass::freeUpMemory();
+
+        $findModel_1 = TestPluginModelClass::findById(1);
+        $findModel_2 = TestAnotherPluginModelClass::findById(1);
+
+        $this->assertInstanceOf('Leadvertex\Plugin\Components\Db\Components\TestPluginModelClass', $findModel_1);
+        $this->assertEquals(1, $findModel_1->getId());
+        $this->assertEquals(1, $findModel_1->value_1);
+        $this->assertEquals('1', $findModel_1->value_2);
+
+        $this->assertInstanceOf('Leadvertex\Plugin\Components\Db\Components\TestAnotherPluginModelClass', $findModel_2);
+        $this->assertEquals(1, $findModel_2->getId());
+        $this->assertEquals(2, $findModel_2->value_1);
+        $this->assertEquals('2', $findModel_2->value_2);
+    }
+
+    public function testAddTwoSinglePluginModelAndFind()
+    {
+        $model_1 = new TestSinglePluginModelClass();
+        $model_1->value_1 = 1;
+        $model_1->value_2 = '1';
+        $model_1->save();
+
+        $model_2 = new TestAnotherSinglePluginModelClass();
+        $model_2->value_1 = 2;
+        $model_2->value_2 = '2';
+        $model_2->save();
+
+        TestModelClass::freeUpMemory();
+
+        $findModel_1 = TestSinglePluginModelClass::find();
+        $findModel_2 = TestAnotherSinglePluginModelClass::find();
+
+        $this->assertInstanceOf('Leadvertex\Plugin\Components\Db\Components\TestSinglePluginModelClass', $findModel_1);
+        $this->assertEquals(2, $findModel_1->getId());
+        $this->assertEquals(1, $findModel_1->value_1);
+        $this->assertEquals('1', $findModel_1->value_2);
+
+        $this->assertInstanceOf('Leadvertex\Plugin\Components\Db\Components\TestAnotherSinglePluginModelClass', $findModel_2);
+        $this->assertEquals(2, $findModel_2->getId());
+        $this->assertEquals(2, $findModel_2->value_1);
+        $this->assertEquals('2', $findModel_2->value_2);
     }
 
 }
